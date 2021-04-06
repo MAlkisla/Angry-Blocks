ddae858f8c4b6f507810a63fb9674ec0d03cedd5
Meriç ALKIŞLA <mericalkisla@gmail.com>
2021-04-06 12:42:15 +0300

    destroying blocks with cannons

--- Angry Blocks/Assets/Scripts/Gameplay Scripts/GameController.cs
+++ Angry Blocks/Assets/Scripts/Gameplay Scripts/GameController.cs
@@ -12,11 +12,13 @@ public class GameController : MonoBehaviour
     private Vector2 level1Pos;
     private Vector2 level2Pos;
 
+    public GameObject[] block;
+
     void Start()
     {
         Physics2D.gravity = new Vector2(0, -17);
 
-        SpawnNewLevel(0, 17);
+        SpawnNewLevel(0, 17, 3, 5);
     }
 
     void Update()
@@ -24,7 +26,7 @@ public class GameController : MonoBehaviour
         
     }
 
-    void SpawnNewLevel(int numberLevel1, int numberLevel2)
+    void SpawnNewLevel(int numberLevel1, int numberLevel2, int min, int max)
     {
         level1Pos = new Vector2(3.5f, 1f);
         level2Pos = new Vector2(3.5f, -3.4f);
@@ -34,5 +36,18 @@ public class GameController : MonoBehaviour
 
         Instantiate(level1, level1Pos, Quaternion.identity);
         Instantiate(level2, level2Pos, Quaternion.identity);
+
+        SetBlocksCount(min, max);
+    }
+
+    void SetBlocksCount(int min, int max)
+    {
+        block = GameObject.FindGameObjectsWithTag("Block");
+
+        for (int i = 0; i < block.Length; i++)
+        {
+            int count = Random.Range(min, max);
+            block[i].GetComponent<Block>().SetStartingCount(count);
+        }
     }
 }
