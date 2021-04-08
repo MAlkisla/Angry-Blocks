bcbc121a699ee81c7ff364929f8a1254d3afab44
Meriç ALKIŞLA <mericalkisla@gmail.com>
2021-04-08 12:50:51 +0300

    Level change and tag error in some blocks fix

--- Angry Blocks/Assets/Scripts/Gameplay Scripts/GameController.cs
+++ Angry Blocks/Assets/Scripts/Gameplay Scripts/GameController.cs
@@ -16,14 +16,15 @@ public class GameController : MonoBehaviour
 
     void Start()
     {
+        PlayerPrefs.DeleteKey("Level");
         Physics2D.gravity = new Vector2(0, -17);
 
-        SpawnNewLevel(0, 17, 3, 5);
+        SpawnLevel();
     }
 
     void Update()
     {
-        
+        CheckBlocks();
     }
 
     void SpawnNewLevel(int numberLevel1, int numberLevel2, int min, int max)
@@ -48,6 +49,63 @@ public class GameController : MonoBehaviour
         {
             int count = Random.Range(min, max);
             block[i].GetComponent<Block>().SetStartingCount(count);
+        }
+    }
+
+    void SpawnLevel()
+    {
+        if (PlayerPrefs.GetInt("Level", 0) == 0)
+            SpawnNewLevel(0, 17, 3, 5);
+
+        if (PlayerPrefs.GetInt("Level") == 1)
+            SpawnNewLevel(12, 21, 4, 6);
+
+        if (PlayerPrefs.GetInt("Level") == 2)
+            SpawnNewLevel(8, 18, 6, 8);
+
+        if (PlayerPrefs.GetInt("Level") == 3)
+            SpawnNewLevel(4, 14, 3, 5);
+
+        if (PlayerPrefs.GetInt("Level") == 4)
+            SpawnNewLevel(5, 13, 4, 6);
+
+        if (PlayerPrefs.GetInt("Level") == 5)
+            SpawnNewLevel(26, 30, 6, 8);
+
+        if (PlayerPrefs.GetInt("Level") == 6)
+            SpawnNewLevel(40, 44, 3, 5);
+
+        if (PlayerPrefs.GetInt("Level") == 7)
+            SpawnNewLevel(42, 22, 3, 5);
+
+        if (PlayerPrefs.GetInt("Level") == 8)
+            SpawnNewLevel(38, 28, 6, 8);
+
+        if (PlayerPrefs.GetInt("Level") == 9)
+            SpawnNewLevel(41, 16, 3, 5);
+
+        if (PlayerPrefs.GetInt("Level") == 10)
+            SpawnNewLevel(36, 26, 4, 6);
+    }
+
+    void CheckBlocks()
+    {
+        block = GameObject.FindGameObjectsWithTag("Block");
+
+        if (block.Length < 1)
+        {
+            RemoveBalls();
+            PlayerPrefs.SetInt("Level", PlayerPrefs.GetInt("Level") + 1);
+            SpawnLevel();
+        }
+    }
+
+    void RemoveBalls()
+    {
+        GameObject[] balls = GameObject.FindGameObjectsWithTag("Ball");
+        for (int i = 0; i < balls.Length; i++)
+        {
+            Destroy(balls[i]);
         }
     }
 }
